2a564a81215d9f444cae7ae68374e2c6aff0f977
William Martínez <WILLIAMSAN78@LIVE.COM>
2024-06-21 02:01:06 -0600

    retorno

--- src/main.js
+++ src/main.js
@@ -6,9 +6,9 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 
 //url del backend recuerda quitar cualquier pleca del final / quitarla
-axios.defaults.baseURL= process.env.BACKEND || "https://backazure.williammartinez.me"
+//axios.defaults.baseURL= process.env.BACKEND || "https://backazure.williammartinez.me"
 //axios.defaults.baseURL= process.env.BACKEND || "http://backend.io"
-//axios.defaults.baseURL = process.env.BACKEND || "https://ejemplobackendueswilliam-77513eb5c189.herokuapp.com"
+axios.defaults.baseURL = process.env.BACKEND || "https://ejemplobackendueswilliam-77513eb5c189.herokuapp.com"
 
 
 Vue.use(VueAxios, axios)
